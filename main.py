5ffb4c147cc2054e98ae56cefb7d2e8bf8e44179
Dima2709 <87536237+Dima2709@users.noreply.github.com>
2021-10-10 18:06:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import os
+#url = 'https://www.kinopoisk.ru/lists/series-top250/'
+
+#req = requests.get(url)
+
+#a = req.text
+
+#with open ("index.html", "w", encoding='utf-8') as file:
+#    file.write(a)
+
+with open("index.html", encoding = "utf-8") as file:
+    kin = file.read()
+
+soup = BeautifulSoup(kin, 'lxml')
+
+table = soup.find(class_= "selection-list").find_all(class_= "desktop-rating-selection-film-item")
+name = []
+year = []
+year1 = []
+country = []
+genre = []
+rating = []
+count_rating = []
+for i in table:
+    table1 = i.find_all(class_="selection-film-item-meta__name")
+    name.append(table1[0].text)
+for i in table:
+    table1 = i.find_all(class_="selection-film-item-meta__original-name")
+    year.append(table1[0].text)
+for i in table:
+    table1 = i.find_all(class_="selection-film-item-meta__meta-additional-item")
+    country.append(table1[0].text)
+for i in table:
+    table1 = i.find_all(class_="rating__value rating__value_positive")
+    rating.append(table1[0].text)
+for i in table:
+    table1 = i.find_all(class_="selection-film-item-meta__meta-additional")
+    genre.append(table1[0].text)
+for i in table:
+    table1 = i.find_all(class_="rating__count")
+    count_rating.append(table1[0].text)
+for i in year:
+    year1.append(i.split()[-1])
+
+
+with open(f"data.csv", "a", encoding="utf-8") as file:
+           writer = csv.writer(file,delimiter=';',lineterminator='\n')
+           table1 = zip(name,year1,country,genre,rating,count_rating)
+           for row in table1:
+               writer.writerow(row)
+
