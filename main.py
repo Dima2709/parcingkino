1c45f2e7b3a44245afb43e42176868df2584c2ea
Dima2709 <87536237+Dima2709@users.noreply.github.com>
2021-10-11 16:09:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import os
-#url = 'https://www.kinopoisk.ru/lists/series-top250/'
+url = 'https://www.kinopoisk.ru/lists/series-top250/'
 
-#req = requests.get(url)
+req = requests.get(url)
 
-#a = req.text
+a = req.text
 
-#with open ("index.html", "w", encoding='utf-8') as file:
-#    file.write(a)
+with open ("index.html", "w", encoding='utf-8') as file:
+    file.write(a)
 
 with open("index.html", encoding = "utf-8") as file:
     kin = file.read()
@@ -55,3 +55,60 @@ with open(f"data.csv", "a", encoding="utf-8") as file:
            for row in table1:
                writer.writerow(row)
 
+for k in range(2,6):
+
+
+     url = "https://www.kinopoisk.ru/lists/series-top250/?"
+
+     req = requests.get(url + f"page={k}&"+"tab=all")
+
+     a = req.text
+
+     with open ("index.html", "w", encoding='utf-8') as file:
+        file.write(a)
+
+     with open("index.html", encoding = "utf-8") as file:
+        kin = file.read()
+
+     soup = BeautifulSoup(kin, 'lxml')
+
+     print(soup.text)
+
+     table = soup.find(class_= "selection-list").find_all(class_= "desktop-rating-selection-film-item")
+     name = []
+     year = []
+     year1 = []
+     country = []
+     genre = []
+     rating = []
+     count_rating = []
+     for i in table:
+         table1 = i.find_all(class_="selection-film-item-meta__name")
+         name.append(table1[0].text)
+     for i in table:
+         table1 = i.find_all(class_="selection-film-item-meta__original-name")
+         year.append(table1[0].text)
+     for i in table:
+         table1 = i.find_all(class_="rating__value rating__value_positive")
+         rating.append(table1[0].text)
+     for i in table:
+         table1 = i.find_all('p',class_="selection-film-item-meta__meta-additional")
+         for j in table1:
+             count = 0
+             for t in j:
+                if count % 2 == 0:
+                   country.append(t.text)
+                   count += 1
+                elif count % 2 != 0:
+                   genre.append(t.text)
+                   count += 1
+     for i in table:
+         table1 = i.find_all(class_="rating__count")
+         count_rating.append(table1[0].text)
+     for i in year:
+         year1.append(i.split()[-1])
+     with open(f"data.csv", "a", encoding="utf-8") as file:
+           writer = csv.writer(file,delimiter=';',lineterminator='\n')
+           table1 = zip(name,year1,country,genre,rating,count_rating)
+           for row in table1:
+               writer.writerow(row)
