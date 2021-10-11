7d3444d4b1865e53c75108ca850da28615d39d9d
Dima2709 <87536237+Dima2709@users.noreply.github.com>
2021-10-11 12:54:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ soup = BeautifulSoup(kin, 'lxml')
 table = soup.find(class_= "selection-list").find_all(class_= "desktop-rating-selection-film-item")
 name = ['name']
 year = []
-year1 = ['year1']
+year1 = ['year']
 country = ['country']
-genre = ['country_genre']
+genre = ['genre']
 rating = ['rating']
 count_rating = ['count_rating']
 for i in table:
@@ -31,24 +31,27 @@ for i in table:
     table1 = i.find_all(class_="selection-film-item-meta__original-name")
     year.append(table1[0].text)
 for i in table:
-    table1 = i.find_all(class_="selection-film-item-meta__meta-additional-item")
-    country.append(table1[0].text)
-for i in table:
     table1 = i.find_all(class_="rating__value rating__value_positive")
     rating.append(table1[0].text)
 for i in table:
-    table1 = i.find_all(class_="selection-film-item-meta__meta-additional")
-    genre.append(table1[0].text)
+    table1 = i.find_all('p',class_="selection-film-item-meta__meta-additional")
+    for j in table1:
+        count = 0
+        for t in j:
+            if count % 2 == 0:
+                country.append(t.text)
+                count += 1
+            elif count % 2 != 0:
+                genre.append(t.text)
+                count += 1
 for i in table:
     table1 = i.find_all(class_="rating__count")
     count_rating.append(table1[0].text)
 for i in year:
     year1.append(i.split()[-1])
-
-
 with open(f"data.csv", "a", encoding="utf-8") as file:
            writer = csv.writer(file,delimiter=';',lineterminator='\n')
-           table1 = zip(name,year1,genre,rating,count_rating)
+           table1 = zip(name,year1,country,rating,count_rating)
            for row in table1:
                writer.writerow(row)
 
